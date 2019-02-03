19783ce30ef8c37fc5135b1a7306a8cdc03fe4a2
OlexandrHorch <olexandr_horch@ukr.net>
2019-02-03 21:12:17 +0200

    Java Developer_HomeWork_3

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import menu.StartMenu;
+
+public class App {
+    public static void main(String[] args) {
+        new StartMenu();
+    }
+}
---
+++ src/main/java/dao/CompanyDAO.java
@@ -0,0 +1,59 @@
+package dao;
+
+import entity.Company;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import service.Connection;
+
+import java.util.List;
+
+public class CompanyDAO {
+
+    public void createCompany(Company company) {
+        Session session = Connection.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(company);
+        transaction.commit();
+        session.close();
+    }
+
+
+    public Company getCompanyById(long companyId) {
+        Company company;
+
+        Session session = Connection.getSessionFactory().openSession();
+        company = session.get(Company.class, companyId);
+        session.close();
+
+        return company;
+    }
+
+
+    public List<Company> getAllCompanies() {
+        List<Company> result;
+
+        Session session = Connection.getSessionFactory().openSession();
+        result = session.createQuery("FROM Company", Company.class).list();
+        session.close();
+
+        return result;
+    }
+
+
+    public void updateCompany(Company company) {
+        Session session = Connection.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(company);
+        transaction.commit();
+        session.close();
+    }
+
+
+    public void deleteCompany(Company company) {
+        Session session = Connection.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(company);
+        transaction.commit();
+        session.close();
+    }
+}
